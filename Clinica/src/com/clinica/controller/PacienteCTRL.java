0425cc47672cc6c6089238a8cceeafefccc7de78
Jonathan <33239505+jonathanssm@users.noreply.github.com>
2019-03-30 18:12:15 -0300

    Add files via upload

--- Clinica/src/com/clinica/controller/PacienteCTRL.java
+++ Clinica/src/com/clinica/controller/PacienteCTRL.java
@@ -5,24 +5,42 @@
  */
 package com.clinica.controller;
 
+import com.clinica.dal.Conexao;
 import com.clinica.dal.PacienteDAL;
 import com.clinica.model.Paciente;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  *
  * @author Jonathan
  */
 public class PacienteCTRL {
-    
+
     private Paciente p;
     private PacienteDAL pd;
-    
-    public void add(String nome, int rg, int cpf, String data_nasc, String pSaude, String email, String sexo) throws SQLException{
-        
+
+    private Conexao con = new Conexao();
+
+    public void fomartDate(String data) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            sdf.setLenient(false);
+            sdf.parse(data);
+            sdf.format(sdf.parse(data));
+        } catch (ParseException ex) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Data Invalida", "ERRO", javax.swing.JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    public void add(long cpf, int rg, String nome, String data_nasc, String pSaude, String email, String sexo, String endereco, String bairro, String numero, int CEP, String celular, String telefone) throws SQLException {
+
         p = new Paciente();
-        pd = new PacienteDAL();
-        
+        pd = new PacienteDAL(con);
+
         p.setNome(nome);
         p.setRg(rg);
         p.setCpf(cpf);
@@ -30,9 +48,20 @@ public class PacienteCTRL {
         p.setpSaude(pSaude);
         p.setEmail(email);
         p.setSexo(sexo);
-        
+        p.getTelefone().setCelular(celular);
+        p.getEndereco().setEndereco(endereco);
+        p.getEndereco().setBairro(bairro);
+        p.getEndereco().setNumero(numero);
+        p.getEndereco().setCep(CEP);
+
+        if (telefone.equals("")) {
+            telefone = null;
+        } else {
+            p.getTelefone().setTelefone(telefone);
+        }
+
         pd.add(p);
-        
+
     }
-    
+
 }
